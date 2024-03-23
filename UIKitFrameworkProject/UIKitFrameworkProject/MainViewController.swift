2d27f89270f0f02ee4f0fbc99884d9ae725ce5a7
Sarah Clark <sarahireneclark@gmail.com>
2024-03-23 16:43:42 -0500

    Added an action to a button in code

--- UIKitFrameworkProject/UIKitFrameworkProject/MainViewController.swift
+++ UIKitFrameworkProject/UIKitFrameworkProject/MainViewController.swift
@@ -9,16 +9,14 @@ import UIKit
 
 class MainViewController: UIViewController {
     var counter: Int = 0
+    @IBOutlet weak var sarahButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        let sarahButton = UIButton(configuration: .filled(), primaryAction: UIAction(title: "Press Here", handler:  {[unowned self] action in
-            print("Value of the counter is \(counter)")
-            counter += 1
-        }))
-        sarahButton.frame = CGRect(x: 100, y: 200, width: 150, height: 50)
-        view.addSubview(sarahButton)
+        sarahButton.addAction(UIAction(handler: {
+            [unowned self] action in
+            print("The value of the counter is: \(counter)")
+        }), for: .touchUpInside)
     }
-
 }

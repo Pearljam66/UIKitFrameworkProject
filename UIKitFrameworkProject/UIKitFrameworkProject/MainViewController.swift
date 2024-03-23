88c5ff3335c5b3f4c376eb88e9b4eef86c06f6db
Sarah Clark <sarahireneclark@gmail.com>
2024-03-23 15:14:17 -0500

    Added a button from code rather than created in the interface builder

--- UIKitFrameworkProject/UIKitFrameworkProject/MainViewController.swift
+++ UIKitFrameworkProject/UIKitFrameworkProject/MainViewController.swift
@@ -8,35 +8,22 @@
 import UIKit
 
 class MainViewController: UIViewController {
-
-    @IBOutlet weak var addButton: UIButton!
-    @IBOutlet weak var removeButton: UIButton!
-    @IBOutlet weak var firstLabel: UILabel!
     var counter: Int = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        addButton.configurationUpdateHandler = { [unowned self] button in
-            var current = button.configuration
-            current?.title = "Add Me"
-            current?.showsActivityIndicator = counter > 0 ? true : false
-            current?.imagePlacement = .trailing
-            current?.imagePadding = 15
-            button.configuration = current
-        }
-    }
+        var buttonConfiguration = UIButton.Configuration.tinted()
+        buttonConfiguration.title = "Press me!"
+        buttonConfiguration.subtitle = "Dooo iiiiitt"
+        buttonConfiguration.baseBackgroundColor = .systemCyan
 
-    @IBAction func addToCounter(_ sender: UIButton) {
-        counter += 1
-        firstLabel.text = "Pressed: \(counter)"
-    }
-
-    @IBAction func removeFromCounter(_ sender: UIButton) {
-        counter -= 1
-        if counter >= 0 {
-            firstLabel.text = "Pressed: \(counter)"
-            addButton.setNeedsUpdateConfiguration()
-        }
+        let sarahButton = UIButton(configuration: buttonConfiguration, primaryAction: UIAction(handler: {
+            [unowned self] action in
+            print("Value of counter is: \(counter)")
+            counter += 1
+        }))
+        sarahButton.frame = CGRect(x: 100, y: 200, width: 150, height: 50)
+        view.addSubview(sarahButton)
     }
 
 }

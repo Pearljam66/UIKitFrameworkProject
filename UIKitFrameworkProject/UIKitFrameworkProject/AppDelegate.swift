c0669e3e63533a5974553a58abbde098512b03e0
Sarah Clark <sarahireneclark@gmail.com>
2024-03-23 17:18:12 -0500

    Removed basicSalary variable from the AppDelegate

--- UIKitFrameworkProject/UIKitFrameworkProject/AppDelegate.swift
+++ UIKitFrameworkProject/UIKitFrameworkProject/AppDelegate.swift
@@ -9,7 +9,6 @@ import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-    var basicSalary: Double!
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         return true

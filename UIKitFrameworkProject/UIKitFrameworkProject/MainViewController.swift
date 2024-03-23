a1f192691834080a2a227322c0c28679881702fc
Sarah Clark <sarahireneclark@gmail.com>
2024-03-23 15:04:01 -0500

    Updated a buttons configuration to add an activity counter when greater than 0

--- UIKitFrameworkProject/UIKitFrameworkProject/MainViewController.swift
+++ UIKitFrameworkProject/UIKitFrameworkProject/MainViewController.swift
@@ -10,33 +10,33 @@ import UIKit
 class MainViewController: UIViewController {
 
     @IBOutlet weak var addButton: UIButton!
-
     @IBOutlet weak var removeButton: UIButton!
     @IBOutlet weak var firstLabel: UILabel!
     var counter: Int = 0
 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        addButton.configurationUpdateHandler = { [unowned self] button in
+            var current = button.configuration
+            current?.title = "Add Me"
+            current?.showsActivityIndicator = counter > 0 ? true : false
+            current?.imagePlacement = .trailing
+            current?.imagePadding = 15
+            button.configuration = current
+        }
+    }
+
     @IBAction func addToCounter(_ sender: UIButton) {
         counter += 1
         firstLabel.text = "Pressed: \(counter)"
-
-        var addButtonConfiguration = addButton.configuration
-        addButtonConfiguration?.title = "Adding \(counter)"
-        addButton.configuration = addButtonConfiguration
     }
 
     @IBAction func removeFromCounter(_ sender: UIButton) {
         counter -= 1
         if counter >= 0 {
             firstLabel.text = "Pressed: \(counter)"
-        } else {
-            var removeButtonConfiguration = removeButton.configuration
-            removeButtonConfiguration?.title = "Can't press this!"
-            removeButton.configuration = removeButtonConfiguration
+            addButton.setNeedsUpdateConfiguration()
         }
     }
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-    }
 }

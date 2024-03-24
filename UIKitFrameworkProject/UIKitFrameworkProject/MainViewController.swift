0216aca4239cb1ee62de0328ee296fba1b7c26db
Sarah Clark <sarahireneclark@gmail.com>
2024-03-24 11:08:07 -0500

    Created an Activity Indicator

--- UIKitFrameworkProject/UIKitFrameworkProject/MainViewController.swift
+++ UIKitFrameworkProject/UIKitFrameworkProject/MainViewController.swift
@@ -9,14 +9,25 @@ import UIKit
 
 class MainViewController: UIViewController {
 
-    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var activity: UIActivityIndicatorView!
+    @IBOutlet weak var button: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        progressBar.progress = 0.75
-        progressBar.tintColor = UIColor.systemBlue
-        progressBar.trackTintColor = UIColor.systemCyan
+        // Indicates whether the button tracks a selection, either through a menu or a toggle.
+        button.changesSelectionAsPrimaryAction = true
+        activity.color = .systemRed
+        activity.style = .large
     }
 
+    @IBAction func turnOnAndOff(_ sender: UIButton) {
+        if activity.isAnimating {
+            activity.stopAnimating()
+            button.setTitle("Turn On", for: .normal)
+        } else {
+            activity.startAnimating()
+            button.setTitle("Turn Off", for: .selected)
+        }
+    }
 }

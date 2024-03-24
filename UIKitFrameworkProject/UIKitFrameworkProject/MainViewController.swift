8c8911ad7df76b5d603cd4d95684422c95ed2100
Sarah Clark <sarahireneclark@gmail.com>
2024-03-24 10:44:43 -0500

    Creating a progress bar

--- UIKitFrameworkProject/UIKitFrameworkProject/MainViewController.swift
+++ UIKitFrameworkProject/UIKitFrameworkProject/MainViewController.swift
@@ -9,32 +9,14 @@ import UIKit
 
 class MainViewController: UIViewController {
 
-    @IBOutlet var buttons: [UIButton]!
-    
+    @IBOutlet weak var progressBar: UIProgressView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        progressBar.progress = 0.75
+        progressBar.tintColor = UIColor.systemBlue
+        progressBar.trackTintColor = UIColor.systemCyan
     }
-
-    @IBAction func sayHello(_ sender: UIButton) {
-        print("Hello!")
-    }
-
-    @IBAction func sayGoodbye(_ sender: UIButton) {
-        print("Goodbye!")
-    }
-    
-    @IBAction func disableButtons(_ sender: UIButton) {
-        for button in buttons {
-            button.isEnabled = false
-        }
-    }
-
-    @IBAction func enableButtons(_ sender: UIButton) {
-        for button in buttons {
-            button.isEnabled = true
-        }
-    }
-    
 
 }

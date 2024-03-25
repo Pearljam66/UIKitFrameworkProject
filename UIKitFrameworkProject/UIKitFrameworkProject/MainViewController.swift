59e58bd5738fee084af34bc3310c2e28dcbd43bf
Sarah Clark <sarahireneclark@gmail.com>
2024-03-25 07:20:55 -0500

    Added actions to a segmented control

--- UIKitFrameworkProject/UIKitFrameworkProject/MainViewController.swift
+++ UIKitFrameworkProject/UIKitFrameworkProject/MainViewController.swift
@@ -10,6 +10,16 @@ import UIKit
 class MainViewController: UIViewController {
 
     @IBOutlet weak var myTitle: UILabel!
+    @IBOutlet weak var options: UISegmentedControl!
+
+    override func viewDidLoad() {
+        options.setAction(UIAction(title: "Black", image: nil, handler: { action in
+            print("Black pressed")
+        }), forSegmentAt: 0)
+        options.setAction(UIAction(title: "Red", image: nil, handler: { action in
+            print("Red pressed")
+        }), forSegmentAt: 1)
+    }
 
     @IBAction func changeOption(_ sender: UISegmentedControl) {
         if sender.selectedSegmentIndex == 0 {

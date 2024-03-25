55215684514e0171e4e21bd127821c84966a30b7
Sarah Clark <sarahireneclark@gmail.com>
2024-03-25 12:15:02 -0500

    Added code to change the slider and make the progress view change with it

--- UIKitFrameworkProject/UIKitFrameworkProject/MainViewController.swift
+++ UIKitFrameworkProject/UIKitFrameworkProject/MainViewController.swift
@@ -8,23 +8,14 @@
 import UIKit
 
 class MainViewController: UIViewController {
-
-    @IBOutlet weak var switchValueLabel: UILabel!
-    @IBOutlet weak var sarahSwitch: UISwitch!
+    @IBOutlet weak var progress: UIProgressView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        let colorBackground = UIColor(red: 0.9, green: 0.9, blue: 1.0, alpha: 1.0)
-        let colorButton = UIColor(red: 0.5, green: 0.5, blue: 1.0, alpha: 1.0)
-        sarahSwitch.onTintColor = colorBackground
-        sarahSwitch.thumbTintColor = colorButton
     }
 
-    @IBAction func flipSwitch(_ sender: UISwitch) {
-        if sender.isOn {
-            switchValueLabel.textColor = .systemPurple
-        } else {
-            switchValueLabel.textColor = .label
-        }
+    @IBAction func updateProgress(_ sender: UISlider) {
+        progress.progress = sender.value
     }
+
 }

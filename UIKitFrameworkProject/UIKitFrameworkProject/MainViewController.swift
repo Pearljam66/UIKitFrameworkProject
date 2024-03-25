a3194bd4ef598fcbcd21d408ad3a1f5b21477c92
Sarah Clark <sarahireneclark@gmail.com>
2024-03-25 07:59:56 -0500

    Added a switch that changes the color of the label when you turn it on

--- UIKitFrameworkProject/UIKitFrameworkProject/MainViewController.swift
+++ UIKitFrameworkProject/UIKitFrameworkProject/MainViewController.swift
@@ -9,35 +9,17 @@ import UIKit
 
 class MainViewController: UIViewController {
 
-    @IBOutlet weak var myTitle: UILabel!
-    @IBOutlet weak var options: UISegmentedControl!
+    @IBOutlet weak var switchValueLabel: UILabel!
+
 
     override func viewDidLoad() {
-        options.setAction(UIAction(title: "Black", image: nil, handler: { action in
-            print("Black pressed")
-        }), forSegmentAt: 0)
-        options.setAction(UIAction(title: "Red", image: nil, handler: { action in
-            print("Red pressed")
-        }), forSegmentAt: 1)
-
-        let total = options.numberOfSegments
-        options.insertSegment(action: UIAction(title: "Blue", image: nil, handler: { action in
-            print("Blue Pressed")
-        }), at: total, animated: false)
-
     }
 
-    @IBAction func changeOption(_ sender: UISegmentedControl) {
-        switch sender.selectedSegmentIndex {
-            case 0:
-                myTitle.textColor = .label
-            case 1:
-                myTitle.textColor = .systemRed
-            case 2:
-                myTitle.textColor = .systemBlue
-            default:
-                myTitle.textColor = .label
+    @IBAction func flipSwitch(_ sender: UISwitch) {
+        if sender.isOn {
+            switchValueLabel.textColor = .systemRed
+        } else {
+            switchValueLabel.textColor = .label
         }
     }
-
 }

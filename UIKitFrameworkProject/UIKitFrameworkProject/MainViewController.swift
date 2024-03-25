e863e0e2b922e950ee97402d0059724e4da19c51
Sarah Clark <sarahireneclark@gmail.com>
2024-03-25 12:23:09 -0500

    Added custome values for the slider

--- UIKitFrameworkProject/UIKitFrameworkProject/MainViewController.swift
+++ UIKitFrameworkProject/UIKitFrameworkProject/MainViewController.swift
@@ -9,13 +9,22 @@ import UIKit
 
 class MainViewController: UIViewController {
     @IBOutlet weak var progress: UIProgressView!
+    @IBOutlet weak var slider: UISlider!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        slider.minimumValue = 0.0
+        slider.maximumValue = 10.0
+        slider.value = 0.0
+
+        slider.minimumTrackTintColor = .systemRed
+        slider.maximumTrackTintColor = .systemYellow
     }
 
     @IBAction func updateProgress(_ sender: UISlider) {
-        progress.progress = sender.value
+        let currentValue = sender.value / 10
+        progress.progress = currentValue
     }
 
 }

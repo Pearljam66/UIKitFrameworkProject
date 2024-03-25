dd2df340fd0bcbbf3efa30e6ffaee63423d1798e
Sarah Clark <sarahireneclark@gmail.com>
2024-03-25 08:26:13 -0500

    Added code to change the color of the switch

--- UIKitFrameworkProject/UIKitFrameworkProject/MainViewController.swift
+++ UIKitFrameworkProject/UIKitFrameworkProject/MainViewController.swift
@@ -10,14 +10,19 @@ import UIKit
 class MainViewController: UIViewController {
 
     @IBOutlet weak var switchValueLabel: UILabel!
-
+    @IBOutlet weak var sarahSwitch: UISwitch!
 
     override func viewDidLoad() {
+        super.viewDidLoad()
+        let colorBackground = UIColor(red: 0.9, green: 0.9, blue: 1.0, alpha: 1.0)
+        let colorButton = UIColor(red: 0.5, green: 0.5, blue: 1.0, alpha: 1.0)
+        sarahSwitch.onTintColor = colorBackground
+        sarahSwitch.thumbTintColor = colorButton
     }
 
     @IBAction func flipSwitch(_ sender: UISwitch) {
         if sender.isOn {
-            switchValueLabel.textColor = .systemRed
+            switchValueLabel.textColor = .systemPurple
         } else {
             switchValueLabel.textColor = .label
         }

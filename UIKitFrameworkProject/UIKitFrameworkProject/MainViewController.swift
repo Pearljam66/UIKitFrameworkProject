90323c999436b90ea70a336fd7e007eacb14e139
Sarah Clark <sarahireneclark@gmail.com>
2024-03-26 12:58:15 -0500

    Added the view back

--- UIKitFrameworkProject/UIKitFrameworkProject/MainViewController.swift
+++ UIKitFrameworkProject/UIKitFrameworkProject/MainViewController.swift
@@ -7,42 +7,12 @@
 
 import UIKit
 
-class MainViewController: UIViewController, UITextFieldDelegate {
-    @IBOutlet weak var titleLabel: UILabel!
-    @IBOutlet weak var titleInput: UITextField!
-    @IBOutlet weak var subtitleInput: UITextField!
+class MainViewController: UIViewController {
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // assigns the MainViewController as the delegate of the titleInput text field.
-        titleInput.delegate = self
-        subtitleInput.delegate = self
     }
 
-    @IBAction func changeTitle(_ sender: UIButton) {
-        if titleInput.text != "" && subtitleInput.text != "" {
-            titleLabel.text = titleInput.text! + " - " + subtitleInput.text!
-            titleInput.text = ""
-            subtitleInput.text = ""
-        }
-    }
-
-    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
-        var maximum = 0
-
-        if textField.tag == 1 {
-            maximum = 10
-        } else {
-            maximum = 15
-        }
-
-        if let text = textField.text {
-            let total = text.count + string.count - range.length
-            if total > maximum {
-                return false
-            }
-        }
-        return true
-    }
 
 }

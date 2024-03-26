dbeb950e599fe8a939d0e209419bbdc0f2c701fe
Sarah Clark <sarahireneclark@gmail.com>
2024-03-26 10:47:54 -0500

    Identify two UITextFields by their tags

--- UIKitFrameworkProject/UIKitFrameworkProject/MainViewController.swift
+++ UIKitFrameworkProject/UIKitFrameworkProject/MainViewController.swift
@@ -19,20 +19,30 @@ class MainViewController: UIViewController, UITextFieldDelegate {
         subtitleInput.delegate = self
     }
 
-    func textFieldDidBeginEditing(_ textField: UITextField) {
-        textField.backgroundColor = .systemRed
-    }
-
-    func textFieldDidEndEditing(_ textField: UITextField) {
-        textField.backgroundColor = .white
-    }
-
     @IBAction func changeTitle(_ sender: UIButton) {
         if titleInput.text != "" && subtitleInput.text != "" {
             titleLabel.text = titleInput.text! + " - " + subtitleInput.text!
             titleInput.text = ""
             subtitleInput.text = ""
         }
+    }
+
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        var maximum = 0
+
+        if textField.tag == 1 {
+            maximum = 10
+        } else {
+            maximum = 15
+        }
+
+        if let text = textField.text {
+            let total = text.count + string.count - range.length
+            if total > maximum {
+                return false
+            }
+        }
+        return true
     }
 
 }

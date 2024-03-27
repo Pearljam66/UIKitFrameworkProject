7b85fce5d373179e61bc61057829d207944d40ad
Sarah Clark <sarahireneclark@gmail.com>
2024-03-27 08:36:13 -0500

    Added UITextView

--- UIKitFrameworkProject/UIKitFrameworkProject/MainViewController.swift
+++ UIKitFrameworkProject/UIKitFrameworkProject/MainViewController.swift
@@ -8,11 +8,23 @@
 import UIKit
 
 class MainViewController: UIViewController {
-    
+
+    @IBOutlet weak var message: UITextView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-
+    @IBAction func selection(_ sender: UIButton) {
+        if let text = message.attributedText {
+            if let attrText = try? AttributedString(text, including: \.uiKit) {
+                if let newRange = Range(message.selectedRange, in: attrText) {
+                    var newText = attrText
+                    newText[newRange].foregroundColor = .systemRed
+                    message.attributedText = NSAttributedString(newText)
+                }
+            }
+        }
+    }
+    
 }

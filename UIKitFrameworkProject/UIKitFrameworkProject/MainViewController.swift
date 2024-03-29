1afb204b9a3e2f41b399e081ac8832928d425612
Sarah Clark <sarahireneclark@gmail.com>
2024-03-29 13:25:23 -0500

    Removed some firstResponder code

--- UIKitFrameworkProject/UIKitFrameworkProject/MainViewController.swift
+++ UIKitFrameworkProject/UIKitFrameworkProject/MainViewController.swift
@@ -25,6 +25,6 @@ class MainViewController: UIViewController {
 
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         super.touchesBegan(touches, with: event)
-        titleInput.resignFirstResponder()
+        view.endEditing(true)
     }
 }

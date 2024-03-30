5b56b0e8e430d374ce89d56a8f9d94b48b615998
Sarah Clark <sarahireneclark@gmail.com>
2024-03-30 15:10:34 -0500

    Created a UIPickerView and provided string values

--- UIKitFrameworkProject/UIKitFrameworkProject/MainViewController.swift
+++ UIKitFrameworkProject/UIKitFrameworkProject/MainViewController.swift
@@ -7,24 +7,34 @@
 
 import UIKit
 
-class MainViewController: UIViewController {
+class MainViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    @IBOutlet weak var showYear: UILabel!
+    @IBOutlet weak var pickerYears: UIPickerView!
 
-    @IBOutlet weak var titleLabel: UILabel!
-    @IBOutlet weak var titleInput: UITextField!
+    var years:[String]!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        pickerYears.delegate = self
+        pickerYears.dataSource = self
+        years = ["1944", "1945", "1946", "1947", "1948", "1949", "1950"]
     }
 
-    @IBAction func changeTitle(_ sender: UIButton) {
-        if titleInput.text != "" {
-            titleLabel.text = titleInput.text
-            titleInput.text = ""
-        }
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
     }
 
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        super.touchesBegan(touches, with: event)
-        view.endEditing(true)
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return years.count
     }
+
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return years[row]
+    }
+
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        let year = years[row]
+        showYear.text = "The year is: \(year)"
+    }
+
 }

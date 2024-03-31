533228b58ba09826fcbb77ba6423cfd46b7b94bc
Sarah Clark <sarahireneclark@gmail.com>
2024-03-31 17:24:40 -0500

    Added a background pattern to the main view

--- UIKitFrameworkProject/UIKitFrameworkProject/MainViewController.swift
+++ UIKitFrameworkProject/UIKitFrameworkProject/MainViewController.swift
@@ -8,26 +8,12 @@
 import UIKit
 
 class MainViewController: UIViewController {
-    @IBOutlet weak var datePicker: UIDatePicker!
-    @IBOutlet weak var date: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        let calendar = Calendar.current
-        var components = DateComponents()
-        components.year = 2010
-        components.month = 8
-        components.day = 13
-
-        if let newDate = calendar.date(from: components) {
-            datePicker.date = newDate
+        if let myPattern = UIImage(named: "Oranges") {
+            view.backgroundColor = UIColor(patternImage: myPattern)
         }
-    }
-
-    @IBAction func getDate(_ sender: UIButton) {
-        let selectedDate = datePicker.date
-        let format = selectedDate.formatted(date: .abbreviated, time: .omitted)
-        date.text = "Date: \(format)"
     }
 
 }

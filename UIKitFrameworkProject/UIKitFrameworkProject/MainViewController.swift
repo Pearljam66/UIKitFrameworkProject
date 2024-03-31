1452732591d28aaf0d2c2dc9c3327b7b63d0dd00
Sarah Clark <sarahireneclark@gmail.com>
2024-03-31 16:06:16 -0500

    Display the selected date in the UIDatePicker

--- UIKitFrameworkProject/UIKitFrameworkProject/MainViewController.swift
+++ UIKitFrameworkProject/UIKitFrameworkProject/MainViewController.swift
@@ -7,49 +7,18 @@
 
 import UIKit
 
-class MainViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
-    @IBOutlet weak var showYear: UILabel!
-    @IBOutlet weak var pickerYears: UIPickerView!
-
-    var years:[String]!
-    var cities:[String]!
+class MainViewController: UIViewController {
+    @IBOutlet weak var datePicker: UIDatePicker!
+    @IBOutlet weak var date: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        pickerYears.delegate = self
-        pickerYears.dataSource = self
-        years = ["1944", "1945", "1946", "1947", "1948", "1949", "1950"]
-        cities = ["Mountain View", "Sunnyvale", "Cupertino", "Santa Clara"]
-
-        if let index = years.firstIndex(of: "1945") {
-            pickerYears.selectRow(index, inComponent: 0, animated: false)
-        }
     }
 
-    @IBAction func getYear(_ sender: UIButton) {
-        let rowCity = pickerYears.selectedRow(inComponent: 0)
-        let rowYear = pickerYears.selectedRow(inComponent: 1)
-        showYear.text = "The year is: \(years[rowYear]) in \(cities[rowCity])"
-    }
-
-    func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        return 2
-    }
-
-    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        if component == 0 {
-            return cities.count
-        } else {
-            return years.count
-        }
-    }
-
-    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        if component == 0 {
-            return cities[row]
-        } else {
-            return years[row]
-        }
+    @IBAction func getDate(_ sender: UIButton) {
+        let selectedDate = datePicker.date
+        let format = selectedDate.formatted(date: .abbreviated, time: .omitted)
+        date.text = "Date: \(format)"
     }
 
 }

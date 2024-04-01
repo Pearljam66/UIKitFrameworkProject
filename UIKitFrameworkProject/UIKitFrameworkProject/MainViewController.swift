c5ac8f75a79560bcc3f47fd17df5bfdd20b8cedf
Sarah Clark <sarahireneclark@gmail.com>
2024-03-31 19:35:36 -0500

    Reducing the size of an image

--- UIKitFrameworkProject/UIKitFrameworkProject/MainViewController.swift
+++ UIKitFrameworkProject/UIKitFrameworkProject/MainViewController.swift
@@ -10,24 +10,20 @@ import UIKit
 class MainViewController: UIViewController {
 
     @IBOutlet weak var pictureView: UIImageView!
+
     var currentActive: Bool = false
     var door: UIImage!
     var husky: UIImage!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        door = UIImage(named: "door")
         husky = UIImage(named: "husky")
     }
 
     @IBAction func changePicture(_ sender: UIButton) {
-        if currentActive {
-            pictureView.image = husky
-        } else {
-            pictureView.image = door
-        }
-
-        currentActive.toggle()
+        let thumbnail = husky.preparingThumbnail(of: CGSize(width: 100, height: 100))
+        pictureView.contentMode = .top
+        pictureView.image = thumbnail
     }
 
 }

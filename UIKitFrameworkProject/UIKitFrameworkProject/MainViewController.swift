2bb03d97bb8cc5c1f8057c9399dbdccf6493d666
Sarah Clark <sarahireneclark@gmail.com>
2024-04-01 12:58:10 -0500

    Interface with a visual effect view

--- UIKitFrameworkProject/UIKitFrameworkProject/MainViewController.swift
+++ UIKitFrameworkProject/UIKitFrameworkProject/MainViewController.swift
@@ -9,21 +9,9 @@ import UIKit
 
 class MainViewController: UIViewController {
 
-    @IBOutlet weak var pictureView: UIImageView!
-
-    var currentActive: Bool = false
-    var door: UIImage!
-    var husky: UIImage!
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        husky = UIImage(named: "husky")
-    }
 
-    @IBAction func changePicture(_ sender: UIButton) {
-        let thumbnail = husky.preparingThumbnail(of: CGSize(width: 100, height: 100))
-        pictureView.contentMode = .top
-        pictureView.image = thumbnail
     }
 
 }

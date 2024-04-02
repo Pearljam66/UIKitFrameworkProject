6b1fe1d812d4fc44e0fa3061cfaaea3af8f53625
Sarah Clark <sarahireneclark@gmail.com>
2024-04-02 12:18:42 -0500

    Added a UIScrollView in code

--- UIKitFrameworkProject/UIKitFrameworkProject/MainViewController.swift
+++ UIKitFrameworkProject/UIKitFrameworkProject/MainViewController.swift
@@ -9,9 +9,18 @@ import UIKit
 
 class MainViewController: UIViewController {
 
+    @IBOutlet weak var stackContainer: UIStackView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        let imageView = UIImageView(image: UIImage(named: "doll"))
+        let imageWidth = imageView.frame.size.width
+        let imageHeight = imageView.frame.size.height
 
+        let mainScrollView = UIScrollView(frame: .zero)
+        mainScrollView.contentSize = CGSize(width: imageWidth, height: imageHeight)
+        mainScrollView.addSubview(imageView)
+        stackContainer.addArrangedSubview(mainScrollView)
     }
 
 }

b98567b935e73d6f0a5e4932e8d0560bf52e646a
Sarah Clark <sarahireneclark@gmail.com>
2024-04-02 17:41:26 -0500

    Added content size instead of multiplied

--- UIKitFrameworkProject/UIKitFrameworkProject/MainViewController.swift
+++ UIKitFrameworkProject/UIKitFrameworkProject/MainViewController.swift
@@ -21,22 +21,22 @@ class MainViewController: UIViewController {
         let scrollHeight = stackContainer.frame.size.height
 
         let mainScrollView = UIScrollView(frame: .zero)
-        mainScrollView.contentSize = CGSize(width: scrollWidth + CGFloat(images.count), height: scrollHeight)
+        mainScrollView.contentSize = CGSize(width: scrollWidth * CGFloat(images.count), height: scrollHeight)
         mainScrollView.contentInsetAdjustmentBehavior = .never
         mainScrollView.isPagingEnabled = true
 
         var posX: CGFloat = 0
 
         for image in images {
-            let imageView = UIImageView(frame: CGRect(x: posX, y: 0, width: scrollWidth, height: scrollHeight))
-            imageView.image = UIImage(named: image)
-            imageView.contentMode = .scaleAspectFill
-            imageView.clipsToBounds = true
+            let imgView = UIImageView(frame: CGRect(x: posX, y: 0, width: scrollWidth, height: scrollHeight))
+            imgView.image = UIImage(named: image)
+            imgView.contentMode = .scaleAspectFill
+            imgView.clipsToBounds = true
 
-            mainScrollView.addSubview(imageView)
+            mainScrollView.addSubview(imgView)
             posX = posX + scrollWidth
         }
-        stackContainer.addArrangedSubview(mainScrollView)
 
+        stackContainer.addArrangedSubview(mainScrollView)
     }
 }

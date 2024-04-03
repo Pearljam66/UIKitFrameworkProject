48de66cd6ac0e67d9a1fddb3668e21633179481e
Sarah Clark <sarahireneclark@gmail.com>
2024-04-03 15:58:47 -0500

    Adopting the content area to a new orientation.

--- UIKitFrameworkProject/UIKitFrameworkProject/MainViewController.swift
+++ UIKitFrameworkProject/UIKitFrameworkProject/MainViewController.swift
@@ -15,6 +15,7 @@ class MainViewController: UIViewController, UIScrollViewDelegate {
     var mainScrollView: UIScrollView!
     var imageViews: [UIImageView] = []
     var page: Int = 0
+    var rotating = false
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,50 +26,83 @@ class MainViewController: UIViewController, UIScrollViewDelegate {
 
         view.setNeedsLayout()
         view.layoutIfNeeded()
-        let scrollWidth = stackContainer.frame.size.width
-        let scrollHeight = stackContainer.frame.size.height
 
         mainScrollView = UIScrollView(frame: .zero)
-        mainScrollView.contentSize = CGSize(width: scrollWidth * CGFloat(images.count), height: scrollHeight)
         mainScrollView.contentInsetAdjustmentBehavior = .never
         mainScrollView.isPagingEnabled = true
         mainScrollView.delegate = self
 
-        var posX: CGFloat = 0
         for image in images {
-            let childScrollView = UIScrollView(frame: CGRect(x: posX, y: 0, width: scrollWidth, height: scrollHeight))
-            childScrollView.contentSize = CGSize(width: scrollWidth, height: scrollHeight)
+            let childScrollView = UIScrollView(frame: .zero)
             childScrollView.contentInsetAdjustmentBehavior = .never
             childScrollView.minimumZoomScale = 1.0
             childScrollView.maximumZoomScale = 4.0
             childScrollView.delegate = self
 
-            let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: scrollWidth, height: scrollHeight))
+            let imageView = UIImageView(frame:.zero)
             imageView.image = UIImage(named: image)
-            imageView.contentMode = .scaleAspectFill
+            imageView.contentMode = .scaleAspectFit
             imageView.clipsToBounds = true
             imageViews.append(imageView)
 
             childScrollView.addSubview(imageView)
             mainScrollView.addSubview(childScrollView)
-            posX = posX + scrollWidth
         }
         stackContainer.addArrangedSubview(mainScrollView)
-    }
-    func scrollViewDidScroll(_ scrollView: UIScrollView) {
-        let pageWidth = stackContainer.frame.size.width
-        let getPage = round(mainScrollView.contentOffset.x / pageWidth)
 
-        let currentPage = Int(getPage)
-        if currentPage != page {
-            let scroll = imageViews[page].superview as! UIScrollView
-            scroll.setZoomScale(1.0, animated: true)
-            page = Int(currentPage)
-            pageCounter.currentPage = page
+        updateSize()
+    }
+
+    func updateSize() {
+        let scrollWidth = stackContainer.frame.size.width
+        let scrollHeight = stackContainer.frame.size.height
+
+        var posX: CGFloat = 0
+
+        for imageView in imageViews {
+            let scroll = imageView.superview as! UIScrollView
+            scroll.frame = CGRect(x: posX, y: 0, width: scrollWidth, height: scrollHeight)
+            scroll.contentSize = CGSize(width: scrollWidth, height: scrollHeight)
+            imageView.frame = CGRect(x: 0, y: 0, width: scrollWidth, height: scrollHeight)
+            posX = posX + scrollWidth
+        }
+
+        mainScrollView.contentSize = CGSize(width: scrollWidth * CGFloat(imageViews.count), height: scrollHeight)
+
+        let scrollView = imageViews[page].superview as! UIScrollView
+        mainScrollView.contentOffset = CGPoint(x: scrollView.frame.origin.x, y: 0)
+    }
+
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        if !rotating {
+            let pageWidth = stackContainer.frame.size.width
+            let getPage = round(mainScrollView.contentOffset.x / pageWidth)
+            let currentPage = Int(getPage)
+
+            if currentPage != page {
+                let scroll = imageViews[page].superview as! UIScrollView
+                scroll.setZoomScale(1.0, animated: true)
+                page = Int(currentPage)
+                pageCounter.currentPage = page
+            }
         }
     }
 
     func viewForZooming(in scrollView: UIScrollView) -> UIView? {
         return imageViews[page]
+    }
+
+    override func viewWillTransition(to size: CGSize, with coordinator: any UIViewControllerTransitionCoordinator) {
+        super.viewWillTransition(to: size, with: coordinator)
+
+        rotating = true
+        coordinator.animate(alongsideTransition: nil, completion: {
+            (context: UIViewControllerTransitionCoordinatorContext!) in
+            let scroll = self.imageViews[self.page].superview as! UIScrollView
+            scroll.setZoomScale(1.0, animated: true)
+
+            self.updateSize()
+            self.rotating = false
+        })
     }
 }

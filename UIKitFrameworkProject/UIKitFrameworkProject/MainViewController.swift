42845d0b9f740b8a6102057a070d3e489ede8434
Sarah Clark <sarahireneclark@gmail.com>
2024-04-04 18:55:17 -0500

    Added unwind segue and slider related changes

--- UIKitFrameworkProject/UIKitFrameworkProject/MainViewController.swift
+++ UIKitFrameworkProject/UIKitFrameworkProject/MainViewController.swift
@@ -8,9 +8,15 @@
 import UIKit
 
 class MainViewController: UIViewController {
+    var ratingHusky: Int = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+
+    @IBAction func goBack(_ segue: UIStoryboardSegue) {
+        let controller = segue.source as! SecondViewController
+        ratingHusky = controller.rating
     }
 
 }
--- UIKitFrameworkProject/UIKitFrameworkProject/SecondViewController.swift
+++ UIKitFrameworkProject/UIKitFrameworkProject/SecondViewController.swift
@@ -8,22 +8,17 @@
 import UIKit
 
 class SecondViewController: UIViewController {
+    @IBOutlet weak var sliderRating: UISlider!
+    var rating: Int = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        sliderRating.value = Float(rating)
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func changeRating(_ sender: UISlider) {
+        let value = round(sender.value)
+        sliderRating.value = value
+        rating = Int(value)
     }
-    */
-
 }

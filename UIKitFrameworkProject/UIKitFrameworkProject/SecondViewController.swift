11e3848924145cff2bc703d48753efc4be8a99fc
Sarah Clark <sarahireneclark@gmail.com>
2024-04-08 18:25:14 -0500

    Deactivated large titles for navigationItem's from code

---
+++ UIKitFrameworkProject/UIKitFrameworkProject/SecondViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SecondViewController.swift
+//  UIKitFrameworkProject
+//
+//  Created by Sarah Clark on 4/8/24.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.title = "Second"
+        navigationItem.largeTitleDisplayMode = .never
+    }
+
+}

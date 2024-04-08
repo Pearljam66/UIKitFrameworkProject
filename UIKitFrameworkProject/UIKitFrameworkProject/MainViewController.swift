06e1f82f7beda437464eb7faf30c20311c8f01a9
Sarah Clark <sarahireneclark@gmail.com>
2024-04-08 16:14:11 -0500

    Added code to show the data

--- UIKitFrameworkProject/UIKitFrameworkProject/MainViewController.swift
+++ UIKitFrameworkProject/UIKitFrameworkProject/MainViewController.swift
@@ -25,12 +25,32 @@ class MainViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDa
         selectedPicture = 0
     }
 
+    @IBAction func getPicture(_ sender: UIButton) {
+        selectedPicture = pickerPictures.selectedRow(inComponent: 0)
+        performSegue(withIdentifier: "showPicture", sender: self)
+    }
+
+    @IBAction func goBack(_ segue: UIStoryboardSegue) {
+        let controller = segue.source as! SecondViewController
+        ratings[selectedPicture] = controller.rating
+    }
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let controller = segue.destination as! SecondViewController
+        controller.rating = ratings[selectedPicture]
+        controller.picture = picturesList[selectedPicture]
+    }
+
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 1
     }
 
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
         return picturesList.count
+    }
+
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return picturesList[row]
     }
 
 }
--- UIKitFrameworkProject/UIKitFrameworkProject/SecondViewController.swift
+++ UIKitFrameworkProject/UIKitFrameworkProject/SecondViewController.swift
@@ -8,22 +8,27 @@
 import UIKit
 
 class SecondViewController: UIViewController {
+    @IBOutlet weak var sliderRating: UISlider!
+    @IBOutlet weak var pictureView: UIImageView!
+
+    var rating: Int = 0
+    var picture: String!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        sliderRating.value = Float(rating)
+        pictureView.image = UIImage(named: picture.lowercased())
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func changeRating(_ sender: UISlider) {
+        let value = round(sender.value)
+        sliderRating.value = value
+        rating = Int(value)
     }
-    */
+
+    @IBAction func goBack(_ sender: UIButton) {
+        performSegue(withIdentifier: "goBack", sender: self)
+    }
 
 }

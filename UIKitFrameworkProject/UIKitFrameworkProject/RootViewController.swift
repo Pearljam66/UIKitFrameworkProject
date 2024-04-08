e7bd12ff58601798b3647bbd64d0cdc38de28e10
Sarah Clark <sarahireneclark@gmail.com>
2024-04-08 17:33:33 -0500

    The Navigation bar gets removed when the user taps the screen

--- UIKitFrameworkProject/UIKitFrameworkProject/RootViewController.swift
+++ UIKitFrameworkProject/UIKitFrameworkProject/RootViewController.swift
@@ -10,7 +10,26 @@ import UIKit
 class RootViewController: UIViewController {
 
     override func viewDidLoad() {
+        super.viewDidLoad()
+        var container = AttributeContainer()
+        container.font = UIFont.preferredFont(forTextStyle: .title1)
+        container.foregroundColor = UIColor.white
 
+        let standard = UINavigationBarAppearance()
+        standard.backgroundColor = UIColor.systemRed
+
+        if let keys = try? Dictionary(container, including: \.uiKit) {
+            standard.titleTextAttributes = keys
+        }
+
+        let compact = UINavigationBarAppearance()
+        compact.backgroundColor = UIColor.yellow
+
+        let bar = navigationController?.navigationBar
+        bar?.scrollEdgeAppearance = standard
+        bar?.compactScrollEdgeAppearance = compact
+
+        navigationController?.hidesBarsOnTap = true
     }
 
 }

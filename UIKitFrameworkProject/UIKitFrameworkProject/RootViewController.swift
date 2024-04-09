262dbfe05aa6950651d6023e741945173c5fa3d1
Sarah Clark <sarahireneclark@gmail.com>
2024-04-08 19:52:14 -0500

    Added a custom back button for the SecondViewController

--- UIKitFrameworkProject/UIKitFrameworkProject/RootViewController.swift
+++ UIKitFrameworkProject/UIKitFrameworkProject/RootViewController.swift
@@ -11,26 +11,10 @@ class RootViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        var container = AttributeContainer()
-        container.font = UIFont.preferredFont(forTextStyle: .title1)
+        let button = UIBarButtonItem(title: "Close")
+        button.tintColor = UIColor.systemGreen
 
-        let buttonAppearance = UIBarButtonItemAppearance()
-
-        if let textAttributes = try? Dictionary(container, including: \.uiKit) {
-            buttonAppearance.normal.titleTextAttributes = textAttributes
-        }
-
-        let standard = UINavigationBarAppearance()
-        standard.backgroundColor = UIColor.systemGray4
-        standard.backButtonAppearance = buttonAppearance
-        standard.setBackIndicatorImage(UIImage(systemName: "arrow.backward.circle"), transitionMaskImage: UIImage(systemName: "arrow.backward.circle"))
-
-        let compact = UINavigationBarAppearance()
-        compact.backgroundColor = UIColor.yellow
-
-        let bar = navigationController?.navigationBar
-        bar?.scrollEdgeAppearance = standard
-        bar?.compactScrollEdgeAppearance = compact
+        navigationItem.backBarButtonItem = button
     }
 
 }

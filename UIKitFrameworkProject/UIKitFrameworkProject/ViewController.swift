56b43e579cb206eefa92a3d30000a266ad5d1405
Sarah Clark <sarahireneclark@gmail.com>
2024-04-09 20:05:17 -0500

    Created a tab view and updated the badge from the view controller

---
+++ UIKitFrameworkProject/UIKitFrameworkProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UIKitFrameworkProject
+//
+//  Created by Sarah Clark on 4/9/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    var counter = 1
+
+    @IBAction func updateBadge(_ sender: UIButton) {
+        if let item = self.tabBarItem {
+            item.title = "New Weather"
+            item.badgeValue = String(counter)
+            counter += 1
+        }
+    }
+}

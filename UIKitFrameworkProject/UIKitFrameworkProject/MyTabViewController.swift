1c1327933169ed98203ed4dca6bd8d37251a9b4c
Sarah Clark <sarahireneclark@gmail.com>
2024-04-09 20:35:41 -0500

    Configured tab bar items and made it a scrollEdgeAppearance

--- UIKitFrameworkProject/UIKitFrameworkProject/MyTabViewController.swift
+++ UIKitFrameworkProject/UIKitFrameworkProject/MyTabViewController.swift
@@ -12,34 +12,11 @@ class MyTabViewController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        var attributesNormal = AttributeContainer()
-        attributesNormal.foregroundColor = .systemGray
-
-        var attributesSelected = AttributeContainer()
-        attributesSelected.foregroundColor = .systemGreen
-
         let standard = UITabBarAppearance()
-        standard.backgroundColor = .systemGray5
-        standard.stackedLayoutAppearance.normal.iconColor = .systemGray
-        standard.compactInlineLayoutAppearance.normal.iconColor = .systemGray
-        standard.inlineLayoutAppearance.normal.iconColor = .systemGray
-
-        if let attributes = try? Dictionary(attributesNormal, including: \.uiKit) {
-            standard.stackedLayoutAppearance.normal.titleTextAttributes = attributes
-            standard.compactInlineLayoutAppearance.normal.titleTextAttributes = attributes
-            standard.inlineLayoutAppearance.normal.titleTextAttributes = attributes
-        }
-
-        standard.stackedLayoutAppearance.selected.iconColor = .systemGreen
-        standard.compactInlineLayoutAppearance.selected.iconColor = .systemGreen
-        standard.inlineLayoutAppearance.selected.iconColor = .systemGreen
-
-        if let attributes = try? Dictionary(attributesSelected, including: \.uiKit) {
-            standard.stackedLayoutAppearance.selected.titleTextAttributes = attributes
-            standard.compactInlineLayoutAppearance.selected.titleTextAttributes = attributes
-            standard.inlineLayoutAppearance.selected.titleTextAttributes = attributes
-        }
-        self.tabBar.standardAppearance = standard
+        standard.stackedItemPositioning = .centered
+        standard.stackedItemWidth = 50
+        standard.stackedItemSpacing = 100
+        self.tabBar.scrollEdgeAppearance = standard
     }
 
 }

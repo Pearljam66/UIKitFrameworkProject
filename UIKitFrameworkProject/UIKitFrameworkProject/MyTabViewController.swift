14f21c4a65ba9c41cb1d54aafa6589c3f956e110
Sarah Clark <sarahireneclark@gmail.com>
2024-04-09 20:39:17 -0500

    Declare the second scene as the initial screen for the tab view

--- UIKitFrameworkProject/UIKitFrameworkProject/MyTabViewController.swift
+++ UIKitFrameworkProject/UIKitFrameworkProject/MyTabViewController.swift
@@ -12,11 +12,11 @@ class MyTabViewController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        let standard = UITabBarAppearance()
-        standard.stackedItemPositioning = .centered
-        standard.stackedItemWidth = 50
-        standard.stackedItemSpacing = 100
-        self.tabBar.scrollEdgeAppearance = standard
+        let list = viewControllers!
+        let controller = list[0] as! ViewController
+        controller.tabBarItem?.badgeValue = String(20)
+
+        self.selectedIndex = 1
     }
 
 }

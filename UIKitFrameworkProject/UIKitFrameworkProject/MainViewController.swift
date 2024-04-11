f0f28a3345ede54c812ae70e513a31024ba4f24c
Sarah Clark <sarahireneclark@gmail.com>
2024-04-11 18:33:00 -0500

    Added more information to the tableviewcell

--- UIKitFrameworkProject/UIKitFrameworkProject/MainViewController.swift
+++ UIKitFrameworkProject/UIKitFrameworkProject/MainViewController.swift
@@ -15,17 +15,27 @@ class MainViewController: UIViewController {
         super.viewDidLoad()
 
         daTable.register(UITableViewCell.self, forCellReuseIdentifier: "daCell")
+        prepareDataSource()
+        prepareSnapshot()
+    }
+
+    func prepareDataSource() {
         AppData.dataSource = UITableViewDiffableDataSource<Sections, ItemsData.ID>(tableView: daTable) { tableView, indexPath, itemID in
             let cell = tableView.dequeueReusableCell(withIdentifier: "daCell", for: indexPath)
 
             if let item = AppData.items.first(where: { $0.id == itemID}) {
                 var config = cell.defaultContentConfiguration()
                 config.text = item.name
+                config.secondaryText = "\(item.calories) Calories"
+                config.image = UIImage(named: item.image)
+                config.imageProperties.maximumSize = CGSize(width: 40, height: 40)
                 cell.contentConfiguration = config
             }
             return cell
         }
+    }
 
+    func prepareSnapshot() {
         var snapshot = NSDiffableDataSourceSnapshot<Sections, ItemsData.ID>()
         snapshot.appendSections([.main])
         snapshot.appendItems(AppData.items.map({ $0.id }))

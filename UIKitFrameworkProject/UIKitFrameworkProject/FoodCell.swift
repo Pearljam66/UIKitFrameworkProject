c6a068027c923f1eb608d58e783d48461db748a1
Sarah Clark <sarahireneclark@gmail.com>
2024-04-12 16:19:52 -0500

    Added cell with custom padding

--- UIKitFrameworkProject/UIKitFrameworkProject/FoodCell.swift
+++ UIKitFrameworkProject/UIKitFrameworkProject/FoodCell.swift
@@ -14,6 +14,10 @@ class FoodCell: UITableViewCell {
     override func updateConfiguration(using state: UICellConfigurationState) {
         var config = self.defaultContentConfiguration()
         config.text = item.name
+        config.secondaryText = "\(item.calories) Calories"
+        config.image = UIImage(named: item.image)
+        config.imageProperties.maximumSize = CGSize(width: 60, height: 60)
+        config.directionalLayoutMargins = NSDirectionalEdgeInsets(top: 0, leading: 50, bottom: 0, trailing: 0)
         self.contentConfiguration = config
     }
 

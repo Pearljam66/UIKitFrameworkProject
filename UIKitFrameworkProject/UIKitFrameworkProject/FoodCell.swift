489072cc643ab17dac96dd825f80d8a46e7b7b4a
Sarah Clark <sarahireneclark@gmail.com>
2024-04-12 18:10:51 -0500

    Creating a cell with a custom view and deleting that cell

--- UIKitFrameworkProject/UIKitFrameworkProject/FoodCell.swift
+++ UIKitFrameworkProject/UIKitFrameworkProject/FoodCell.swift
@@ -8,17 +8,51 @@
 import UIKit
 
 class FoodCell: UITableViewCell {
-
+    private var customView: UIListContentView!
+    private var customButton: UIButton!
     var item: ItemsData!
 
     override func updateConfiguration(using state: UICellConfigurationState) {
-        var config = self.defaultContentConfiguration()
-        config.text = item.name
-        config.secondaryText = "\(item.calories) Calories"
-        config.image = UIImage(named: item.image)
-        config.imageProperties.maximumSize = CGSize(width: 60, height: 60)
-        config.directionalLayoutMargins = NSDirectionalEdgeInsets(top: 0, leading: 50, bottom: 0, trailing: 0)
-        self.contentConfiguration = config
-    }
+        createViews()
 
+        if let configuration = self.customView.configuration as? UIListContentConfiguration {
+            var config = configuration.updated(for: state)
+            config.text = item.name
+            config.secondaryText = "Calories: \(item.calories)"
+            config.image = UIImage(named: item.image)
+            config.imageProperties.maximumSize = CGSize(width: 60, height: 60)
+            self.customView.configuration = config
+        }
+    }
+    func createViews() {
+        guard contentView.viewWithTag(999) == nil else { return }
+
+        customView = UIListContentView(configuration: .subtitleCell())
+        customView.translatesAutoresizingMaskIntoConstraints = false
+        self.contentView.addSubview(customView)
+
+        customView.topAnchor.constraint(equalTo: self.contentView.topAnchor, constant: 8).isActive = true
+        customView.leadingAnchor.constraint(equalTo: self.contentView.leadingAnchor, constant: 16).isActive = true
+        customView.bottomAnchor.constraint(equalTo: self.contentView.bottomAnchor, constant: -8).isActive = true
+
+        customButton = UIButton(configuration: .plain(), primaryAction: UIAction(image: UIImage(systemName: "trash"), handler: { [unowned self] action in
+            self.eraseItem()
+        }))
+        customButton.translatesAutoresizingMaskIntoConstraints = false
+        customButton.tag = 999
+        self.contentView.addSubview(customButton)
+
+        customButton.leadingAnchor.constraint(equalTo: customView.trailingAnchor, constant: 8).isActive = true
+        customButton.trailingAnchor.constraint(equalTo: self.contentView.trailingAnchor, constant: -16).isActive = true
+        customButton.centerYAnchor.constraint(equalTo: customView.centerYAnchor).isActive = true
+        customButton.widthAnchor.constraint(equalToConstant: 30).isActive = true
+        customButton.heightAnchor.constraint(equalToConstant: 30).isActive = true
+    }
+    func eraseItem() {
+        AppData.items.removeAll(where: { $0.id == item.id })
+
+        var currentSnapshot = AppData.dataSource.snapshot()
+        currentSnapshot.deleteItems([item.id])
+        AppData.dataSource.apply(currentSnapshot)
+    }
 }

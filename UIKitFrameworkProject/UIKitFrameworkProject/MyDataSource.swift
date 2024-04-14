7798822497510a466e03696b75b6befcd91657c2
Sarah Clark <sarahireneclark@gmail.com>
2024-04-14 15:46:34 -0500

    Added the ability to move cells in edit mode

--- UIKitFrameworkProject/UIKitFrameworkProject/MyDataSource.swift
+++ UIKitFrameworkProject/UIKitFrameworkProject/MyDataSource.swift
@@ -13,4 +13,26 @@ class MyDataSource: UITableViewDiffableDataSource<Sections, ItemsData.ID> {
         return true
     }
 
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+
+    override func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        guard sourceIndexPath != destinationIndexPath else { return }
+
+        let item = AppData.items[sourceIndexPath.row]
+        AppData.items.remove(at: sourceIndexPath.row)
+        AppData.items.insert(item, at: destinationIndexPath.row)
+
+        if let itemOrigin = self.itemIdentifier(for: sourceIndexPath), let itemDestination = self.itemIdentifier(for: destinationIndexPath) {
+            var currentSnapshot = self.snapshot()
+            if sourceIndexPath.row > destinationIndexPath.row {
+                currentSnapshot.moveItem(itemOrigin, beforeItem: itemDestination)
+            } else {
+                currentSnapshot.moveItem(itemOrigin, afterItem: itemDestination)
+            }
+            self.apply(currentSnapshot, animatingDifferences: false)
+        }
+    }
+
 }

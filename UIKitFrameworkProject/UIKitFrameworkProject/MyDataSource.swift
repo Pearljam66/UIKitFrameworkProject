ce4c0d4187d2e78fbce20b45b1a6e5316dfc580e
Sarah Clark <sarahireneclark@gmail.com>
2024-04-14 18:14:59 -0500

    Added code to delete cells

--- UIKitFrameworkProject/UIKitFrameworkProject/MyDataSource.swift
+++ UIKitFrameworkProject/UIKitFrameworkProject/MyDataSource.swift
@@ -13,16 +13,24 @@ class MyDataSource: UITableViewDiffableDataSource<Sections.ID, ItemsData.ID> {
         return AppData.sections[section].name
     }
 
-    override func sectionIndexTitles(for tableView: UITableView) -> [String]? {
-        let titles = AppData.sections.map({ $0.name })
-        return titles
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
     }
 
-    override func tableView(_ tableView: UITableView, sectionForSectionIndexTitle title: String, at index: Int) -> Int {
-        if let index = AppData.sections.firstIndex(where: { $0.name == title }) {
-            return index
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == UITableViewCell.EditingStyle.delete {
+            if let itemID = self.itemIdentifier(for: indexPath), let sectionID = self.sectionIdentifier(for: indexPath.section) {
+                AppData.items.removeAll(where: { $0.id == itemID })
+
+                var currentSnapshot = self.snapshot()
+                currentSnapshot.deleteItems([itemID])
+                if currentSnapshot.numberOfItems(inSection: sectionID) <= 0 {
+                    AppData.sections.removeAll(where: { $0.id == sectionID })
+                    currentSnapshot.deleteSections([sectionID])
+                }
+                self.apply(currentSnapshot)
+            }
         }
-        return 0
     }
 
 }

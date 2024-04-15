4cb467f2d99346887ad44c555ccafbc36361f4b8
Sarah Clark <sarahireneclark@gmail.com>
2024-04-14 19:43:57 -0500

    Configuring the Collection View flow layout

--- UIKitFrameworkProject/UIKitFrameworkProject/MyCollectionViewController.swift
+++ UIKitFrameworkProject/UIKitFrameworkProject/MyCollectionViewController.swift
@@ -11,6 +11,11 @@ class MyCollectionViewController: UICollectionViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        let layout = collectionView.collectionViewLayout as! UICollectionViewFlowLayout
+        layout.itemSize = CGSize(width: 100, height: 100)
+        layout.sectionInset = UIEdgeInsets(top: 20, left: 20, bottom: 20, right: 20)
+
         prepareDataSource()
         prepareSnapshot()
     }

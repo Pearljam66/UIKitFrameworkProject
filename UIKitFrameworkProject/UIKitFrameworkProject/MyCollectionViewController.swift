cc7c2891f5c7ddac6f6a54a1f874dbf7a5173a5e
Sarah Clark <sarahireneclark@gmail.com>
2024-04-14 19:55:51 -0500

    Added a compositional layout to the collection view

--- UIKitFrameworkProject/UIKitFrameworkProject/MyCollectionViewController.swift
+++ UIKitFrameworkProject/UIKitFrameworkProject/MyCollectionViewController.swift
@@ -7,15 +7,13 @@
 
 import UIKit
 
-class MyCollectionViewController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
+class MyCollectionViewController: UICollectionViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        collectionView.delegate = self
 
-        let layout = collectionView.collectionViewLayout as! UICollectionViewFlowLayout
-        layout.itemSize = CGSize(width: 100, height: 100)
-        layout.sectionInset = UIEdgeInsets(top: 20, left: 20, bottom: 20, right: 20)
+        let customLayout = createLayout()
+        collectionView.collectionViewLayout = customLayout
 
         prepareDataSource()
         prepareSnapshot()
@@ -26,20 +24,7 @@ class MyCollectionViewController: UICollectionViewController, UICollectionViewDe
             if let item = AppData.items.first(where: { $0.id == itemID }) {
                 cell.picture.image = UIImage(named: item.image)
             }
-
-            cell.configurationUpdateHandler = { cell, state in
-                var backgroundConfig = UIBackgroundConfiguration.listPlainCell().updated(for: state)
-                backgroundConfig.cornerRadius = 10
-
-                if state.isSelected {
-                    backgroundConfig.backgroundColor = .systemPink
-                } else {
-                    backgroundConfig.backgroundColor = .systemBackground
-                }
-                cell.backgroundConfiguration = backgroundConfig
-            }
         }
-
         AppData.dataSource = UICollectionViewDiffableDataSource<Sections, ItemsData.ID>(collectionView: collectionView) { (collection, indexPath, itemID) in
             return collection.dequeueConfiguredReusableCell(using: cellRegistration, for: indexPath, item: itemID)
         }
@@ -52,15 +37,18 @@ class MyCollectionViewController: UICollectionViewController, UICollectionViewDe
         AppData.dataSource.apply(snapshot)
     }
 
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        var width: CGFloat = 146
-        var height: CGFloat = 100
+    func createLayout() -> UICollectionViewCompositionalLayout {
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.5), heightDimension: .fractionalWidth(0.5))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        item.contentInsets = NSDirectionalEdgeInsets(top: 10, leading: 5, bottom: 5, trailing: 10)
 
-        if indexPath.item % 3 == 0 {
-            width = 292
-            height = 200
-        }
-        return CGSize(width: width, height: height)
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalWidth(0.5))
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
+
+        let section = NSCollectionLayoutSection(group: group)
+
+        let layout = UICollectionViewCompositionalLayout(section: section)
+        return layout
     }
 
 }

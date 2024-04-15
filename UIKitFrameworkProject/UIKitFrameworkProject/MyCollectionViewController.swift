e8d4f118a92eb7f9a5e663603aa2630dff293ce6
Sarah Clark <sarahireneclark@gmail.com>
2024-04-14 19:34:57 -0500

    Configured the background of a Collection View cell

--- UIKitFrameworkProject/UIKitFrameworkProject/MyCollectionViewController.swift
+++ UIKitFrameworkProject/UIKitFrameworkProject/MyCollectionViewController.swift
@@ -7,23 +7,39 @@
 
 import UIKit
 
-private let reuseIdentifier = "Cell"
-
 class MyCollectionViewController: UICollectionViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        prepareDataSource()
+        prepareSnapshot()
+    }
 
+    func prepareDataSource() {
         let cellRegistration = UICollectionView.CellRegistration<FoodCell, ItemsData.ID> { cell, indexPath, itemID in
             if let item = AppData.items.first(where: { $0.id == itemID }) {
                 cell.picture.image = UIImage(named: item.image)
+            }
+
+            cell.configurationUpdateHandler = { cell, state in
+                var backgroundConfig = UIBackgroundConfiguration.listPlainCell().updated(for: state)
+                backgroundConfig.cornerRadius = 10
+
+                if state.isSelected {
+                    backgroundConfig.backgroundColor = .systemPink
+                } else {
+                    backgroundConfig.backgroundColor = .systemBackground
+                }
+                cell.backgroundConfiguration = backgroundConfig
             }
         }
 
         AppData.dataSource = UICollectionViewDiffableDataSource<Sections, ItemsData.ID>(collectionView: collectionView) { (collection, indexPath, itemID) in
             return collection.dequeueConfiguredReusableCell(using: cellRegistration, for: indexPath, item: itemID)
         }
+    }
 
+    func prepareSnapshot() {
         var snapshot = NSDiffableDataSourceSnapshot<Sections, ItemsData.ID>()
         snapshot.appendSections([.main])
         snapshot.appendItems(AppData.items.map({ $0.id }))
@@ -31,3 +47,4 @@ class MyCollectionViewController: UICollectionViewController {
     }
 
 }
+
